0f188243ee0dc0075de6e6e3bb3cf5f3807ff750
Khant Loon Thu <fairylychee312@gmail.com>
2025-01-30 16:20:12 +0630

    add spatie media

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\UserController;
+use App\Services\TempMediaService;
 use Illuminate\Support\Facades\Route;
 
 require __DIR__ . '/auth.php';
@@ -15,9 +16,12 @@ Route::get('/dashboard', function () {
 
 Route::middleware('auth')->group(function () {
 
-    Route::get('/tables', function () {
-        return view('table');
-    });
+    // Route::get('/tables', function () {
+    //     return view('table');
+    // });
+
+    Route::post('temp-upload', [TempMediaService::class, 'storeTempFile'])->name('temp.upload');
+    Route::delete('temp-delete', [TempMediaService::class, 'deleteTempFile'])->name('temp.delete');
 
     // Route::get('/users', [UserController::class, 'index'])->name('users.index');
     Route::resource('users', UserController::class)->except('show');
