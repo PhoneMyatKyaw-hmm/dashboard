9cb59be50ad488cf3e15524e0b5c0616e20d6cd6
Khant Loon Thu <fairylychee312@gmail.com>
2025-01-30 10:16:24 +0630

    Update app.blade.php

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -20,8 +20,8 @@
 
     <!-- Page Wrapper -->
     <div x-data="{ open: true }" class="flex min-h-screen overflow-hidden bg-slate-100 dark:bg-gray-900">
-        <x-side-bar ::class="!open ? 'w-[4.5rem]' : 'w-64'"
-            class="fixed top-0 left-0 z-40 h-screen transition-transform -translate-x-full md:translate-x-0 "></x-side-bar>
+        <x-sidebar ::class="!open ? 'w-[4.5rem]' : 'w-64'"
+            class="fixed top-0 left-0 z-40 h-screen transition-transform -translate-x-full md:translate-x-0 "></x-sidebar>
 
         <!-- Page Content -->
         <div class="relative flex flex-col flex-1 overflow-x-hidden overflow-y-auto"
