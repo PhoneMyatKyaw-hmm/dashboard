8be58481326334e58170e91a11745b817b74e34a
Khant Loon Thu <fairylychee312@gmail.com>
2025-01-30 17:28:05 +0630

    Fix table ui

--- resources/views/components/table/action-item.blade.php
+++ resources/views/components/table/action-item.blade.php
@@ -2,7 +2,7 @@
 
 <li>
     <a href="{{ $href }}"
-        class="flex items-center px-4 py-2 space-x-3 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">
+        {{ $attributes->merge(['class' => 'flex items-center px-4 py-2 space-x-3 hover:bg-gray-100']) }}>
         {{ $slot }}
         <span class="mt-1">{{ $title }}</span>
     </a>
